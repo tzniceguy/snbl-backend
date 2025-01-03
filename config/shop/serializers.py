e0c4f3d0956938da57d40ca4fcfdedc7c18dd287
tzniceguy <joeshimbi@gmail.com>
2025-01-03 15:14:19 +0300

    api views and serializer

---
+++ config/shop/serializers.py
@@ -0,0 +1,110 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Customer, Vendor, Product
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 
+                 'phone_number', 'address', 'created_at')
+        read_only_fields = ('created_at',)
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super().create(validated_data)
+        if password:
+            instance.set_password(password)
+            instance.save()
+        return instance
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'user', 'loyalty_points', 'shipping_address', 
+                 'billing_address', 'preferred_payment_method', 'created_at')
+        read_only_fields = ('created_at', 'loyalty_points')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        customer = Customer.objects.create(user=user, **validated_data)
+        return customer
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        for attr, value in user_data.items():
+            setattr(instance.user, attr, value)
+        instance.user.save()
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+class VendorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Vendor
+        fields = ('id', 'user', 'company_name', 'business_address', 
+                 'tax_id', 'description', 'is_active', 'rating', 'created_at')
+        read_only_fields = ('created_at', 'rating')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        vendor = Vendor.objects.create(user=user, **validated_data)
+        return vendor
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        for attr, value in user_data.items():
+            setattr(instance.user, attr, value)
+        instance.user.save()
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+class ProductSerializer(serializers.ModelSerializer):
+    vendor_name = serializers.CharField(source='vendor.company_name', read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'vendor', 'vendor_name', 'description', 
+                 'price', 'category', 'stock', 'sku', 'is_active', 'created_at')
+        read_only_fields = ('created_at',)
+
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Stock cannot be negative.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than zero.")
+        return value
+
+# Nested serializers for detailed views
+class ProductDetailSerializer(ProductSerializer):
+    vendor = VendorSerializer(read_only=True)
+
+class VendorDetailSerializer(VendorSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta(VendorSerializer.Meta):
+        fields = VendorSerializer.Meta.fields + ('products',)
+
+class CustomerDetailSerializer(CustomerSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta(CustomerSerializer.Meta):
+        fields = CustomerSerializer.Meta.fields
--- config/shop/views.py
+++ config/shop/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import get_user_model
+from .models import Customer, Vendor, Product
+from .serializers import (
+    UserSerializer, CustomerSerializer, VendorSerializer, ProductSerializer,
+    CustomerDetailSerializer, VendorDetailSerializer, ProductDetailSerializer
+)
 
-# Create your views here.
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['user__username', 'user__email', 'shipping_address']
+    filterset_fields = ['loyalty_points']
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'me']:
+            return CustomerDetailSerializer
+        return CustomerSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'list']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        customer = self.get_queryset().filter(user=request.user).first()
+        if customer:
+            serializer = self.get_serializer(customer)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['company_name', 'user__email', 'description']
+    filterset_fields = ['is_active', 'rating']
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return VendorDetailSerializer
+        return VendorSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        vendor = self.get_queryset().filter(user=request.user).first()
+        if vendor:
+            serializer = self.get_serializer(vendor)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=True, methods=['GET'])
+    def products(self, request, pk=None):
+        vendor = self.get_object()
+        products = vendor.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.select_related('vendor')
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'sku', 'vendor__company_name']
+    filterset_fields = ['category', 'is_active', 'vendor']
+    ordering_fields = ['price', 'created_at', 'name']
+    ordering = ['-created_at']
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ProductDetailSerializer
+        return ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        vendor = Vendor.objects.get(user=self.request.user)
+        serializer.save(vendor=vendor)
