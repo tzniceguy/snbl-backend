3d0b1d88c1a2ca354e4b294fc2b4c6043ccea6b4
tzniceguy <joeshimbi@gmail.com>
2025-01-04 18:55:49 +0300

    registering the models to admin panel

--- config/shop/admin.py
+++ config/shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User,Customer,Vendor,Product
+from .models import User,Customer,Vendor,Product,Order,Payment
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Vendor)
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Payment)
