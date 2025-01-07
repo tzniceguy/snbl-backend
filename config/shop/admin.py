6290d2d5b8fcc44fe7821e49110acf09d088ab18
tzniceguy <joeshimbi@gmail.com>
2025-01-07 18:35:48 +0300

    added model to admin

--- config/shop/admin.py
+++ config/shop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import User,Customer,Vendor,Product,Order,Payment
+from .models import CustomUser,Customer,Vendor,Product,Order,Payment
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(CustomUser)
 admin.site.register(Customer)
 admin.site.register(Vendor)
 admin.site.register(Product)
