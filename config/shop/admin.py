95f2b2e215dbe9a7e056f24f88f206bc97192756
tzniceguy <joeshimbi@gmail.com>
2025-01-13 09:14:25 +0300

    added product category to the admin

--- config/shop/admin.py
+++ config/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser,Customer,Vendor,Product,Order,Payment
+from .models import CustomUser,Customer, Vendor,Product,Order,Payment,ProductCategory
 # Register your models here.
 
 admin.site.register(CustomUser)
@@ -8,3 +8,4 @@ admin.site.register(Vendor)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Payment)
+admin.site.register(ProductCategory)
