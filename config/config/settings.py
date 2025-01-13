29c2f91f86b5e5381824213ff01854277a6bef14
tzniceguy <joeshimbi@gmail.com>
2025-01-13 13:14:25 +0300

    cors origin set to each host for development

--- config/config/settings.py
+++ config/config/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -170,7 +172,9 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOW_ALL_ORIGINS = True
+
+'''CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://192.168.1.148:3000',
-]
+    'http://192.168.1.162:3000',
+]'''
