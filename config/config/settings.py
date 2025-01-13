d6ae4945d8f068f0435596432f353f975f5a142c
tzniceguy <joeshimbi@gmail.com>
2025-01-13 13:30:34 +0300

    change the media file upload name

--- config/config/settings.py
+++ config/config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
