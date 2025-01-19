19217e5054b48db5cdd9264daef755b40be1121c
tzniceguy <joeshimbi@gmail.com>
2025-01-19 14:13:49 +0300

    static root

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
