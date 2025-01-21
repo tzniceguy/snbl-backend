f138c0052c83b93560a70025990c1af16049a05c
tzniceguy <joeshimbi@gmail.com>
2025-01-21 12:12:49 +0300

    registering orderitem to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser,Customer, Vendor,Product,Order,Payment,ProductCategory
+from .models import CustomUser,Customer, Vendor,Product,Order,Payment,ProductCategory,OrderItem
 # Register your models here.
 
 admin.site.register(CustomUser)
@@ -9,3 +9,4 @@ admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Payment)
 admin.site.register(ProductCategory)
+admin.site.register(OrderItem)
